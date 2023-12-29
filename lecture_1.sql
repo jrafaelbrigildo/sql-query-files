9ca9b881451f7caeef85e55d219faa2b69cb67ce
jrafaelbrigildo <rafaelbrigildo@gmail.com>
2023-12-29 12:47:29 +0800

    After Changes

---
+++ lecture_1.sql
@@ -0,0 +1,25 @@
+insert into students(student_id, first_name, last_name,
+					age, gender, date_of_birth, city)
+VALUES
+  (1, 'John', 'Doe', 20, 'M', '2002-05-15', 'New York'),
+  (2, 'Jane', 'Smith', 22, 'F', '2000-08-23', 'Los Angeles'),
+  (3, 'Michael', 'Johnson', 21, 'M', '2001-02-10', 'Chicago'),
+  (4, 'Emma', 'Williams', 19, 'F', '2003-04-30', 'San Francisco'),
+  (5, 'Daniel', 'Martinez', 23, 'M', '1999-11-18', 'Miami'),
+  (6, 'Sophia', 'Brown', 20, 'F', '2002-09-05', 'Seattle'),
+  (7, 'Ethan', 'Davis', 22, 'M', '2000-06-12', 'Boston'),
+  (8, 'Olivia', 'Miller', 21, 'F', '2001-08-07', 'Dallas'),
+  (9, 'Matthew', 'Garcia', 19, 'M', '2003-01-25', 'Phoenix'),
+  (10, 'Ava', 'Rodriguez', 23, 'F', '1999-07-14', 'Denver'),
+  (11, 'William', 'Hernandez', 20, 'M', '2002-03-22', 'Atlanta'),
+  (12, 'Mia', 'Lopez', 22, 'F', '2000-12-03', 'Philadelphia'),
+  (13, 'Alexander', 'Taylor', 21, 'M', '2001-05-20', 'San Diego'),
+  (14, 'Abigail', 'Clark', 19, 'F', '2003-10-09', 'Houston'),
+  (15, 'James', 'Lee', 23, 'M', '1999-04-17', 'Detroit'),
+  (16, 'Chloe', 'White', 20, 'F', '2002-07-28', 'Minneapolis'),
+  (17, 'Benjamin', 'Scott', 22, 'M', '2000-09-14', 'Portland'),
+  (18, 'Harper', 'Turner', 21, 'F', '2001-12-11', 'Las Vegas'),
+  (19, 'Henry', 'Baker', 19, 'M', '2003-02-08', 'Charlotte'),
+  (20, 'Lily', 'Gomez', 23, 'F', '1999-06-05', 'Orlando');
+  
+select * from students;
